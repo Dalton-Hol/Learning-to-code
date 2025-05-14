27e342a3647971d2fcf4d811a6ba9cdd923bef13
Dalton-Hol <daltonhollanderj+git@gmail.com>
2025-05-14 15:38:46 -0700

    this fucntion is upsetting me will continue later

--- JavaScript Algorithms & Data Structures/PJ 5 validate user input (calorie counter)/script.js
+++ JavaScript Algorithms & Data Structures/PJ 5 validate user input (calorie counter)/script.js
@@ -15,4 +15,14 @@ function isInvalidInput(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
 }
-console.log(isInvalidInput("1e3"))
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>
+    `;
+}
