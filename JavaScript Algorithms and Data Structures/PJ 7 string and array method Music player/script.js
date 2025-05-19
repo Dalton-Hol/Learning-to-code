865b48c5aed9eacb96f70435d9de1ce9e4e73bfe
Dalton-Hol <daltonhollanderj+git@gmail.com>
2025-05-18 20:56:47 -0700

    75% mark

--- JavaScript Algorithms and Data Structures/PJ 7 string and array method Music player/script.js
+++ JavaScript Algorithms and Data Structures/PJ 7 string and array method Music player/script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
     userData.currentSong = song;
     playButton.classList.add("playing");
 
+    highlightCurrentSong();
     audio.play();
 };
 
@@ -129,8 +130,23 @@ const playPreviousSong = () => {
     }
 };
 
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+};
 
-
+const highlightCurrentSong = () => {
+    const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(
+        `song-${userData?.currentSong?.id}`
+    );
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute("aria-current");
+    });
+    if (songToHighlight) {
+        songToHighlight.setAttribute("aria-current", "true");
+    }
+};
 const renderSongs = (array) => {
     const songsHTML = array
         .map((song) => {
