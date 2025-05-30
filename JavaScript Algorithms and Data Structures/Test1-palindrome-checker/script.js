12c75204cf4b2717bd2120d2bbc970cd48e4184b
Dalton-Hol <daltonhollanderj+git@gmail.com>
2025-05-29 20:13:25 -0700

    start of JS entrys

--- JavaScript Algorithms and Data Structures/Test1-palindrome-checker/script.js
+++ JavaScript Algorithms and Data Structures/Test1-palindrome-checker/script.js
@@ -1,1 +1,8 @@
-//start
+const resultMsg = document.getElementById("result");
+const form = document.getElementById("palindrome-form");
+const input = document.getElementById("text-input");
+
+
+
+
+form.addEventListener("submit",/*addfunctionhere*/);
