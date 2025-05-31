4ede04254abff6b29c1109b9b72b6ab75e3463bd
Dalton-Hol <daltonhollanderj+git@gmail.com>
2025-05-30 17:58:08 -0700

    i think this is correct commit, but will check for bugs

--- JavaScript Algorithms and Data Structures/Test1-palindrome-checker/script.js
+++ JavaScript Algorithms and Data Structures/Test1-palindrome-checker/script.js
@@ -1,8 +1,42 @@
 const resultMsg = document.getElementById("result");
 const form = document.getElementById("palindrome-form");
-const input = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
 
 
+function getInput() {
+    const originalInput = document.getElementById("text-input").value;
+    const cleanedInput = cleanInput(originalInput);
+    return {
+        original: originalInput,
+        cleaned: cleanedInput
+    }
+}
+
+function cleanInput(input) {
+    const regex = /[\W_]+/g;
+    return input.replace(regex, '').toLowerCase();
+}
+
+/*function reverseInput(prepedInput.cleaned) {
+    let reversed = "";
+
+    for (let i = prepedInput.cleaned.length - 1; i >= 0; i--) {
+        reversed += prepedInput.cleaned[i];
+    }
+    return reversed;
+} */
+
+function checkPalindrome() {
+    const prepedInput = getInput()
+    //reverseInput(prepedInput.cleaned)
+    const reversed = prepedInput.cleaned.split('').reverse().join('');
+    if (prepedInput.cleaned === reversed) {
+        resultMsg.innerText = `${prepedInput.original} is a palindrome`
+    } else {
+        resultMsg.innerText = `${prepedInput.original} is not a palindrome`
+    }
+}
+
+checkBtn.addEventListener("click", checkPalindrome);
 
 
-form.addEventListener("submit",/*addfunctionhere*/);
